2227273b8ba2ae0cda35435d63ae30c9c5d7a5bd
Sotten8 <stas.oleksiichuk88@gmail.com>
2024-11-04 14:39:46 +0200

    Update functions.js

--- lab3/functions.js
+++ lab3/functions.js
@@ -17,7 +17,7 @@ const generateKey = (length, characters) => {
     let i = 1;
     const arr = [];
     while (i <= length) {
-        arr.push(characters[Math.floor(Math.random() * characters.length)])
+        arr.push(characters[Math.floor(Math.random() * characters.length)]);
         i++;
     };
     return arr.join('');
@@ -26,3 +26,39 @@ const generateKey = (length, characters) => {
 module.exports = { generateKey };
 
 //3
+const ip = "165.225.133.150";
+const dividedArray = ip.split('.');
+const changedIp = () => {
+    let sum = 0;
+    let i = 3;
+    for (const num of dividedArray) {
+        const changedNumber = (num << (8*i));
+        sum += changedNumber;
+        i--;
+        if (i<0) break;
+    };
+    return sum;
+};
+module.exports = { changedIp };
+
+//4
+const iface = {
+    m1: (x,y) => [x,y],
+    m2: function (x) {
+      return [x];
+    },
+    m3 (a, b, c, d) {
+      return [a, b, c, d];
+    }
+  };
+const elements = () => {
+    const array = [];
+    for (const keys in iface) {
+        const fn = iface[keys];
+        if (typeof fn === 'function') {
+            array.push([keys,fn.length]);
+        };
+    };
+    return array;
+};
+module.exports = { elements };
